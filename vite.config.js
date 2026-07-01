70686c26a924e47c20c12676844aa160bee7201b
Hellene Grace E. Septimo <septimogracey@gmail.com>
2026-07-01 16:09:17 +0800

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/portfolio/',
   optimizeDeps: {
     exclude: ['react-icons', 'react-icons/si'],
   },
